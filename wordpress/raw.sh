d7f48af26baa616269e5a17d1414fd91280413ab
Amir Bashiri <bashiri.net@gmail.com>
2022-05-19 21:31:26 +0430

    Add raw bash

--- wordpress/raw.sh
+++ wordpress/raw.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-## change path to root of project
+## Change path to root of project
 cd "$(dirname $0)/../"
 
-## remove wordpress files
+## Remove wordpress files
 rm -rf wordpress/code/*
 find wordpress/code/ -type f -exec rm {} \;
 
+## Download and extract fresh wordpress
 curl https://wordpress.org/latest.tar.gz --output /tmp/wordpress.tar.gz
 tar -xzvf /tmp/wordpress.tar.gz -C wordpress/code/ --strip-components=1
